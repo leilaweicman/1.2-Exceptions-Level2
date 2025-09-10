fe5aed7cedce6b5683ffd44be46c30e47c780fc3
leilaweicman <leilaweic@hotmail.com>
2025-09-10 13:19:45 +0200

    First commit

---
+++ src/Exercise1/Input.java
@@ -0,0 +1,32 @@
+package Exercise1;
+
+public class Input {
+
+    public static byte readByte(String message) {
+        return 0;
+    }
+
+    public static int readInt(String message) {
+        return 0;
+    }
+
+    public static float readFloat(String message) {
+        return 0;
+    }
+
+    public static double readDouble(String message) {
+        return 0;
+    }
+
+    public static char readChar(String message) {
+        return '0';
+    }
+
+    public static String readString(String message) {
+        return "";
+    }
+
+    public static boolean readYesNo(String message) {
+        return false;
+    }
+}
