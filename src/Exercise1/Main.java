663d314f78f8a70fb730b6608b6bb969d828d15c
leilaweicman <leilaweic@hotmail.com>
2025-09-15 11:33:23 +0200

    chore: add Main demo to test Input methods

--- src/Exercise1/Main.java
+++ src/Exercise1/Main.java
@@ -3,12 +3,25 @@ package Exercise1;
 public class Main {
     public static void main(String[] args) {
 
-        byte age1 = Input.readByte("Introduce your age");
+        byte quantity = Input.readByte("Enter the product quantity");
+        System.out.println("Quantity (byte): " + quantity);
 
-        int age2 = Input.readInt("Introduce your age");
+        int year = Input.readInt("Enter the current year");
+        System.out.println("Year (int): " + year);
 
-        float age3 = Input.readFloat("Introduce your age");
+        float temperature = Input.readFloat("Enter the temperature in Celsius");
+        System.out.println("Temperature (float): " + temperature);
 
-        double age4 = Input.readDouble("Introduce your age");
+        double price = Input.readDouble("Enter the product price");
+        System.out.println("Price (double): " + price);
+
+        char initial = Input.readChar("Enter the first letter of your name");
+        System.out.println("Initial (char): " + initial);
+
+        String city = Input.readString("Enter your city");
+        System.out.println("City (string): " + city);
+
+        boolean continueProcess = Input.readYesNo("Do you want to continue?");
+        System.out.println("Continue? " + continueProcess);
     }
 }
